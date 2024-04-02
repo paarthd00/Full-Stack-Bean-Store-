f0566badaf9af8705f917ef22d12caa15fba8c3d
paarthd00 <paarthcode@gmail.com>
2024-04-01 19:11:03 -0700

    Tanstack Router

--- packages/web/src/main.tsx
+++ packages/web/src/main.tsx
@@ -6,11 +6,26 @@ import {
   QueryClient,
   QueryClientProvider,
 } from '@tanstack/react-query'
+
+import { RouterProvider, createRouter } from '@tanstack/react-router'
+
+import { routeTree } from './routeTree.gen'
+
+const router = createRouter({ routeTree })
+
+declare module '@tanstack/react-router' {
+  interface Register {
+    router: typeof router
+  }
+}
+
+
 const queryClient = new QueryClient()
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <App />
+      <RouterProvider router={router} />
     </QueryClientProvider>
   </React.StrictMode>,
 )
---
+++ packages/web/src/routes/index.lazy.tsx
@@ -0,0 +1,47 @@
+import { createLazyFileRoute } from '@tanstack/react-router'
+
+export const Route = createLazyFileRoute('/')({
+  component: Index,
+})
+import { useState, useEffect } from 'react'
+
+import '../App.css'
+
+export default function Index() {
+  const [message, setMessage] = useState("Hi 👋");
+
+  useEffect(() => {
+    (async () => {
+      await fetch(import.meta.env.VITE_APP_API_URL+ "/chat", {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ message: 'Hi' }),
+      })
+      .then(async (response) => {
+        const data = await response.json()
+        console.log(data)
+      }
+      ) 
+    })()
+  }, []);
+
+  function onClick() {
+    fetch(import.meta.env.VITE_APP_API_URL)
+      .then((response) => response.text())
+      .then(setMessage);
+  }
+
+  return (
+    <div className="App">
+      <div className="bg-[#fff] px-3">
+        <button onClick={onClick}>
+          Message is "<i>{message}</i>"
+        </button>
+      </div>
+    </div>
+  );
+}
+
+
